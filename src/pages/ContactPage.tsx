30f2120e73934d7ee9b1a5bcd3e3999cbba46aa9
ELom Laurent <laziagbenyo@gmail.com>
2025-06-23 14:22:35 +0000

    fix responsive

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -12,8 +12,8 @@ const ContactPage = () => {
       <div className="bg-cover bg-center w-full flex flex-col pb-10 bg-opacity-70">
        <HomeNavBar/>
 
-        <div className="container mx-auto flex  pt-8">
-          <div className="flex flex-col w-1/2  text-gray-900">
+        <div className="container mx-auto md:flex  pt-8   text-center md:text-start px-5 md:px-0">
+          <div className="flex flex-col w-full md:w-1/2  text-gray-900">
             <h1 className="font-bebas-neue uppercase text-4xl md:text-6xl font-black text-gray-900 leading-tight mb-6 ">
               Une question ou un souci ? Faites-nous en part
             </h1>
@@ -22,13 +22,13 @@ const ContactPage = () => {
             </p>
             <h2 className="  font-extrabold text-2xl">Ecrivrez nous</h2>
             <p> Notre super equipe est la pour vous</p>
-            <p className="font-extrabold mb-20  "> duffetshopping@gmail.com </p>
-            <span className="flex gap-4">
-              <Instagram />
+            <p className="font-extrabold mb-4  "> duffetshopping@gmail.com </p>
+            <span className="flex gap-4 justify-center mb-20  ">
+              <Instagram  />
               <Facebook />
             </span>
           </div>
-          <form className="space-y-8 w-1/2">
+          <form className="space-y-8 w-full md:w-1/2">
             {/* Nom + Prénom */}
             <div className="flex flex-col md:flex-row gap-8">
               <div className="flex items-center w-full md:w-1/2 py-4 px-4 rounded-full bg-black/10 backdrop-blur-md border border-black/30 text-gray-900 shadow-md">
